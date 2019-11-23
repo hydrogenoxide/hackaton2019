5109a34a987618ce9f46843f92178c1057ac3a9d
Andreas Vollert <acv@mailbox.org>
2019-11-23 14:54:27 +0100

    Add first stage of handling arbitrary many lanes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "xodr/xodr_map.h"
 
+
 class Stl_bin_file{
   struct stl_facet {
     float normal[3];
@@ -63,77 +64,98 @@ int get_poligon(double s_section, std::vector<LaneSection::WidthPoly3>* polygons
   return polygons->size() - 1;
 }
 
+std::vector<Eigen::Vector2d> get_one_vertex_row(LaneSection* laneSection,
+						ReferenceLine* ref_line,
+						double s, double start){
+  std::vector<Eigen::Vector2d> vertex_row;
+  vertex_row.reserve(laneSection->lanes().size());
+
+  // point and tangent direction at the current s we are looking at
+  ReferenceLine::PointAndTangentDir ptd = ref_line->eval(s);
+
+  int offset = laneSection->numRightLanes();
+
+  vertex_row.at(offset) = ptd.pointWithTCoord(0);
+
+  double accumulated_width = 0;
+  // iterate over lanes with negative id
+  for (int i = 1; i <= laneSection->numRightLanes(); i++) {
+    LaneID lid = LaneID(-i);
+    LaneSection::Lane lane = laneSection->laneById(lid);
+    std::vector<LaneSection::WidthPoly3> polygons = lane.widthPoly3s();
+
+    int poly_idx = get_poligon(s - start, &polygons);
+    LaneSection::WidthPoly3 poly = polygons.at(poly_idx);
+    double width = poly.poly3().eval(s - poly.sOffset());
+    accumulated_width += width;
+    vertex_row.at(offset - i) = ptd.pointWithTCoord(accumulated_width);
+  }
+
+  accumulated_width = 0;
+  // iterate over lanes with positive id
+  for (int i = 1; i <= laneSection->numLeftLanes(); i++) {
+    LaneID lid = LaneID(i);
+    LaneSection::Lane lane = laneSection->laneById(lid);
+    std::vector<LaneSection::WidthPoly3> polygons = lane.widthPoly3s();
+
+    int poly_idx = get_poligon(s - start, &polygons);
+    LaneSection::WidthPoly3 poly = polygons.at(poly_idx);
+    double width = poly.poly3().eval(s - poly.sOffset());
+    accumulated_width += width;
+    vertex_row.at(offset + i) = ptd.pointWithTCoord(accumulated_width);
+  }
+  return vertex_row;
+}
+
+std::vector<Eigen::Vector2d[3]> create_mesh(std::vector<Eigen::Vector2d>* vertices_behind,
+					    std::vector<Eigen::Vector2d>* vertices_next){
+  std::vector<Eigen::Vector2d[3]> mesh;
+  mesh.reserve(2 * (vertices_next->size() - 1));
+
+  for (int i = 0; i < vertices_next->size(); ++i) {
+    Eigen::Vector2d tris1[3] = {vertices_next->at(i), vertices_behind->at(i), vertices_behind->at(i + 1)};
+    Eigen::Vector2d tris2[3] = {vertices_next->at(i), vertices_behind->at(i + 1), vertices_next->at(i + 1)};
+    mesh.push_back(tris1);
+    mesh.push_back(tris2);
+  }
+  return mesh;
+}
+
 void convert_stl(XodrMap* xodrMap) {
 
   size_t number_steps = 20;
 
 
   for (const Road& road : xodrMap->roads()) {
+    // std::cerr << "New Road" << std::endl;
+
+
+    ReferenceLine ref_line = road.referenceLine();
+
     for (LaneSection laneSection : road.laneSections()){
 
       double step_size = (laneSection.endS() - laneSection.startS()) / number_steps;
       double start = laneSection.startS();
+      double s = start;
 
-      for (LaneSection::Lane lane : laneSection.lanes()){
-	// woosh
-	if (lane.id().operator int() != 1) {
-	  continue;
-	}
 
-	std::vector<LaneSection::WidthPoly3> polygons = lane.widthPoly3s();
+      std::vector<Eigen::Vector2d> vertices_behind =
+	get_one_vertex_row(&laneSection, &ref_line, s, start);
 
-	double s = start;
-	int poly_idx = get_poligon(s - start, &polygons);
-	if (poly_idx < 0) {
-	  std::cerr << "Something went horribly wrong at the beginning :(" << std::endl;
-	  exit(-1);
-	}
-	LaneSection::WidthPoly3 poly = polygons.at(poly_idx);
-	double width = poly.poly3().eval(s - poly.sOffset());
+      std::vector<std::array<Eigen::Vector2d, 3>> mesh;
 
-	// only true for lane with id 1
-	double t1 = 0;
-	double t2 = width;
+      for (int i = 1; i < number_steps; i++) {
+	s += i * step_size;
+	std::vector<Eigen::Vector2d> vertices_next =
+	  get_one_vertex_row(&laneSection, &ref_line, s, start);
 
-	// that's wrong
-	ReferenceLine::PointAndTangentDir ptd = road.referenceLine().eval(s);
+	std::vector<Eigen::Vector2d[3]> new_tris = create_mesh(&vertices_behind, &vertices_next);
+	mesh.reserve(mesh.size() + new_tris.size());
+	mesh.insert(mesh.end(), new_tris.begin(), new_tris.end());
+	vertices_behind = vertices_next;
 
-	Eigen::Vector2d v1 = ptd.pointWithTCoord(t1);
-	Eigen::Vector2d v2 = ptd.pointWithTCoord(t2);
-
-	// std::cout << "v1 (" << v1.x() << "," << v1.y() << ")" << std::endl;
-	// std::cout << "v2 (" << v2.x() << "," << v2.y() << ")" << std::endl;
-
-        for (int i = 1; i < number_steps; i++) {
-          s = start + i * step_size;
-          poly_idx = get_poligon(s - start, &polygons);
-	  if (poly_idx < 0) {
-	    std::cerr << "Something went horribly wrong :(" << std::endl;
-	    exit(-1);
-	  }
-          poly = polygons.at(poly_idx);
-          width = poly.poly3().eval(s - poly.sOffset());
-
-          t1 = 0;
-          t2 = width;
-
-	  ptd = road.referenceLine().eval(s);
-          Eigen::Vector2d v3 = ptd.pointWithTCoord(t1);
-          Eigen::Vector2d v4 = ptd.pointWithTCoord(t2);
-
-          // pay attention to order because of orientation!
-          tris(v3, v2, v1);
-          tris(v2, v3, v4);
-
-	  // std::cout << "v1 (" << v1.x() << "," << v1.y() << ")" << std::endl;
-	  // std::cout << "v2 (" << v2.x() << "," << v2.y() << ")" << std::endl;
-	  // std::cout << "v3 (" << v3.x() << "," << v3.y() << ")" << std::endl;
-	  // std::cout << "v4 (" << v4.x() << "," << v4.y() << ")" << std::endl;
-
-          v1 = v3;
-          v2 = v4;
-        }
       }
+
     }
   }
 }
