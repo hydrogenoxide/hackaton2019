a224258420dce6b27bc494454ecfb6f0fa1fe05a
Andreas Vollert <acv@mailbox.org>
2019-11-23 11:37:09 +0100

    Something went horribly wrong :(

--- src/main.cpp
+++ src/main.cpp
@@ -25,16 +25,16 @@ static bool showLaneType(LaneType laneType)
     laneType == LaneType::BORDER;
 }
 
-std::string tris(Eigen::Vector2d v1, Eigen::Vector2d v2, Eigen::Vector2d v3) {
+void tris(Eigen::Vector2d v1, Eigen::Vector2d v2, Eigen::Vector2d v3) {
   std::cout << "facet normal 0 0 1" << std::endl;
-	    std::cout << "\touter loop" << std::endl;
-	    std::cout << "\t\tvertex" << v1.x() << " " << v1.y() << " " << "0" << std::endl;
-	    std::cout << "\t\tvertex" << v2.x() << " " << v2.y() << " " << "0" << std::endl;
-	    std::cout << "\t\tvertex" << v3.x() << " " << v3.y() << " " << "0" << std::endl;
-	    std::cout << "\t\tvertex" << std::endl;
-	    std::cout << "\t\tvertex" << std::endl;
-	    std::cout << "\tendloop" << std::endl;
-	    std::cout << "end facet" << std::endl;
+  std::cout << "\touter loop" << std::endl;
+  std::cout << "\t\tvertex" << v1.x() << " " << v1.y() << " " << "0" << std::endl;
+  std::cout << "\t\tvertex" << v2.x() << " " << v2.y() << " " << "0" << std::endl;
+  std::cout << "\t\tvertex" << v3.x() << " " << v3.y() << " " << "0" << std::endl;
+  std::cout << "\t\tvertex" << std::endl;
+  std::cout << "\t\tvertex" << std::endl;
+  std::cout << "\tendloop" << std::endl;
+  std::cout << "end facet" << std::endl;
 }
 
 int get_poligon(double s, std::vector<LaneSection::WidthPoly3>* polygons) {
@@ -46,7 +46,7 @@ int get_poligon(double s, std::vector<LaneSection::WidthPoly3>* polygons) {
       return i;
     }
   }
-  
+  return -1;
 }
 
 void convert_stl(XodrMap* xodrMap) {
@@ -62,14 +62,18 @@ void convert_stl(XodrMap* xodrMap) {
       
       for (LaneSection::Lane lane : laneSection.lanes()){
 	// woosh
-	if (lane.id() != 1) {
+	if (lane.id().operator int() != 1) {
 	  continue;
 	}
 
 	std::vector<LaneSection::WidthPoly3> polygons = lane.widthPoly3s();
 
-	double s = start + i * step_size;
+	double s = start;
 	int poly_idx = get_poligon(s, &polygons);
+	if (poly_idx < 0) {
+	  std::cerr << "Something went horribly wrong at the beginning :(" << std::endl;
+	  exit(-1);
+	}
 	LaneSection::WidthPoly3 poly = polygons.at(poly_idx);
 	double width = poly.poly3().eval(s - poly.sOffset());
 
@@ -83,58 +87,59 @@ void convert_stl(XodrMap* xodrMap) {
 	Eigen::Vector2d v1 = ptd.pointWithTCoord(t1);
 	Eigen::Vector2d v2 = ptd.pointWithTCoord(t2);
 
-		
-	for(int i = 1; i < number_steps, i++) {
-	  s = start + i * step_size;
-	  poly_idx = get_poligon(s, &polygons);
-	  poly = polygons.at(poly_idx);
-	  width = poly.poly3().eval(s - poly.sOffset());
+        for (int i = 1; i < number_steps; i++) {
+          s = start + i * step_size;
+          poly_idx = get_poligon(s, &polygons);
+	  if (poly_idx < 0) {
+	    std::cerr << "Something went horribly wrong :(" << std::endl;
+	    exit(-1);
+	  }
+          poly = polygons.at(poly_idx);
+          width = poly.poly3().eval(s - poly.sOffset());
 
-	  t1 = 0;
-	  t2 = width;
+          t1 = 0;
+          t2 = width;
 
-	  Eigen::Vector2d v3 = ptd.pointWithTCoord(t1);
-	  Eigen::Vector2d v4 = ptd.pointWithTCoord(t2);
+          Eigen::Vector2d v3 = ptd.pointWithTCoord(t1);
+          Eigen::Vector2d v4 = ptd.pointWithTCoord(t2);
 
+          // pay attention to order because of orientation!
+          tris(v3, v2, v1);
+          tris(v2, v3, v4);
 
-	  // pay attention to order because of orientation!
-	  tris(v3, v2, v1);
-	  tris(v2, v3, v4);
-
-	  v1 = v3;
-	  v2 = v4;
-	  
-	}
-
+          v1 = v3;
+          v2 = v4;
+        }
       }
     }
   }
+}
 
+int main() {
+  const char* path = xodrFiles[1].path;
+
+  std::cout << "Loading xodr file: " << path << std::endl;
+
+  XodrParseResult<XodrMap> fromFileRes = XodrMap::fromFile(path);
+
+  if (!fromFileRes.hasFatalErrors())
+    {
+      XodrMap* xodrMap(new XodrMap(std::move(fromFileRes.value())));
+      std::cout << "solid road" << std::endl;
+      convert_stl(xodrMap);
+      std::cout << "endsolid road" << std::endl;
+    }
+  else
+    {
+      std::cout << "Errors: " << std::endl;
+      for (const auto& err : fromFileRes.errors())
+	{
+	  std::cout << err.description() << std::endl;
+	}
+
+      std::cerr << "Failed to load xodr file " << path << std::endl;
+    }
   
-  int main()
-  {
-    const char* path = xodrFiles[1].path;
 
-    std::cout << "Loading xodr file: " << path << std::endl;
-
-    XodrParseResult<XodrMap> fromFileRes = XodrMap::fromFile(path);
-
-    if (!fromFileRes.hasFatalErrors())
-      {
-	XodrMap* xodrMap(new XodrMap(std::move(fromFileRes.value())));
-	bar(xodrMap);
-      }
-    else
-      {
-	std::cout << "Errors: " << std::endl;
-	for (const auto& err : fromFileRes.errors())
-	  {
-	    std::cout << err.description() << std::endl;
-	  }
-
-	std::cerr << "Failed to load xodr file " << path << std::endl;
-      }
-  
-
-    return 0;
-  }
+  return 0;
+}
