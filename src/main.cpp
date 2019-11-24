950121c7af896c72dea20011f0e34feb6828f85a
Andreas Vollert <acv@mailbox.org>
2019-11-24 10:00:10 +0100

    Specify file to convert as command line argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include "triangulation.h"
 #include <iostream>
 
-int main() {
-  std::string path = "data/opendrive/sample1.1.xodr";
+int main(int argc, char *argv[]) {
+
+  if (argc < 2) {
+    std::cout << "No file to convert provided." << std::endl;
+    std::cout << "Usage:" << std::endl;
+    std::cout << argv[0] << " <OpenROAD file>" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // std::string path = "data/opendrive/sample1.1.xodr";
+  char *p = argv[1];
+  std::string path = p;
 
   try {
     SimpleMesh mesh = convert_xodr(path);
